a1488266bb9b88e917a89cc73de5e967f7e317a9
Kesley Ferreira <kesley.beline@gmail.com>
2024-03-07 23:17:40 -0300

    ajustes

--- src/main/java/controller/ContatoBean.java
+++ src/main/java/controller/ContatoBean.java
@@ -1,16 +1,48 @@
 package controller;
 
+import dao.ContatoDAO;
+import jakarta.persistence.PostPersist;
 import model.Contato;
+import org.primefaces.util.Lazy;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @ManagedBean
 @ViewScoped
+@Named
 public class ContatoBean implements Serializable {
 
     private Contato contato = new Contato();
+    private List<Contato> listaContatos;
+    @Inject
+    private ContatoDAO contatoDAO;
+
+    public void listarContatos() {
+        listaContatos = ContatoDAO.listarContatos;
+    }
+
+    public void salvarContato() {
+        contatoDAO.salvarContato(contato);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Contato salvo com sucesso!", null));
+        contato = new Contato();
+    }
+
+    public void editarContato() {
+        contatoDAO.editarContato(contato);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Contato editado com sucesso!", null));
+    }
+
+    public void excluirContato() {
+        contatoDAO.excluirContato(contato);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Contato excluído com sucesso!", null));
+    }
 
     public Contato getContato() {
         return contato;
@@ -18,23 +50,7 @@ public class ContatoBean implements Serializable {
     public void setContato(Contato contato) {
         this.contato = contato;
     }
-
-    public String salvarContato() {
-        // lógica de salvar
-
-        return "Sucesso";
+    public List<Contato> getListaContatos() {
+        return listaContatos;
     }
-
-    public String editarContato() {
-        // lógica de editar
-
-        return "Sucesso";
-    }
-
-    public void excluirContato() {
-        //lógica para excluir do banco
-    }
-
-
-
 }
--- src/main/java/dao/ContatoDAO.java
+++ src/main/java/dao/ContatoDAO.java
@@ -1,15 +1,43 @@
 package dao;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+import model.Contato;
+
+import java.util.List;
 
 public class ContatoDAO {
-   /* protected EntityManager getEnttityManager() {
-        EntityManagerFactory = null;
-        EntityManager entityManager = null;
-        return entityManager;
+    public static List<Contato> listarContatos;
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    public List<Contato> listarContatos(Contato contato) {
+        String jpql = "SELECT c FROM Contato c";
+        TypedQuery<Contato> query = entityManager.createQuery(jpql, Contato.class);
+        return query.getResultList();
     }
 
-    */
+    @Transactional
+    public void salvarContato(Contato contato) {
+        entityManager.persist(contato);
+    }
+
+    @Transactional
+    public void editarContato(Contato contato) {
+        entityManager.merge(contato);
+    }
+
+    @Transactional
+    public void excluirContato(Contato contato) {
+        contato = entityManager.merge(contato);
+        entityManager.remove(contato);
+    }
+
+    @Transactional
+    public void excluirContatoPorId(Integer contatoId) {
+        Contato contato = entityManager.find(Contato.class, contatoId);
+        if (contato != null) {
+            entityManager.remove(contato);
+        }
+    }
 }
